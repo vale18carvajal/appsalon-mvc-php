8abf859f4f09b8737d82badebee4320981e7563f
Valeria Carvajal <valeriacarvajal1@outlook.com>
2024-09-12 14:16:18 -0600

    Página404

--- Router.php
+++ Router.php
@@ -42,11 +42,12 @@ class Router
             // Call user fn va a llamar una función cuando no sabemos cual sera
             call_user_func($fn, $this); // This es para pasar argumentos
         } else {
-            echo "Página No Encontrada o Ruta no válida";
+            //echo "Página No Encontrada o Ruta no válida";
+            $this->render('pagina404',[],false);
         }
     }
 
-    public function render($view, $datos = [])
+    public function render($view, $datos = [], $layout = true)
     {
 
         // Leer lo que le pasamos  a la vista
@@ -54,11 +55,17 @@ class Router
             $$key = $value;  // Doble signo de dolar significa: variable variable, básicamente nuestra variable sigue siendo la original, pero al asignarla a otra no la reescribe, mantiene su valor, de esta forma el nombre de la variable se asigna dinamicamente
         }
 
-        ob_start(); // Almacenamiento en memoria durante un momento...
+        
+        if ($layout) {
+            ob_start(); // Almacenamiento en memoria durante un momento...
 
-        // entonces incluimos la vista en el layout
-        include_once __DIR__ . "/views/$view.php";
-        $contenido = ob_get_clean(); // Limpia el Buffer
-        include_once __DIR__ . '/views/layout.php';
+            // entonces incluimos la vista en el layout
+            include_once __DIR__ . "/views/$view.php";
+            $contenido = ob_get_clean(); // Limpia el Buffer
+            include_once __DIR__ . '/views/layout.php';
+        } else {
+            include_once __DIR__ . "/views/$view.php";
+        }
+        
     }
 }
